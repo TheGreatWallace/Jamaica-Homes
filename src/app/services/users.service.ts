d246bacfbb1a53f7d976fc2d794a6e0f0c21076e
TheGreatWallace <carywallace26@gmail.com>
2022-09-06 11:02:22 -0500

    users controller updated

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -14,7 +14,7 @@ export class UsersService {
 
   constructor(private http: HttpClient) { }
 
-registerUsers(data: any): Observable<any>{
+registerUsers(data: any): Observable<Users>{
   return this.http.post(this.REST_API_URL+"/register", data);
 }
 
